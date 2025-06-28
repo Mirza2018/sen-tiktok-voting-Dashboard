7e603e42263e943f9d2ca86ed02f152d9f88cb1b
mirza2018 <mirza.eee.4th@gmail.com>
2025-06-28 11:11:34 +0600

    suscription

--- src/Components/SuperAdminPages/VotingCandidatePage/AddCandidatesFrom.jsx
+++ src/Components/SuperAdminPages/VotingCandidatePage/AddCandidatesFrom.jsx
@@ -104,7 +104,7 @@ const AddCandidatesFrom = ({
             layout="vertical"
             className="bg-transparent w-full text-start"
           >
-            <Typography.Title level={4} style={{ color: "#222222" }}>
+            {/* <Typography.Title level={4} style={{ color: "#222222" }}>
               Candidate Name
             </Typography.Title>
             <Form.Item
@@ -121,7 +121,94 @@ const AddCandidatesFrom = ({
                 placeholder="Enter Candidate name"
                 className=" px-3 text-xl border !border-input-color !bg-transparent"
               />
-            </Form.Item>
+            </Form.Item> */}
+
+            <div className="flex gap-5 justify-between">
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#000" }}>
+                  Candidate Name
+                </Typography.Title>
+                <Form.Item
+                  name="name"
+                  className=""
+                  rules={[
+                    {
+                      required: true,
+                      message: "Name is Required",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter your name"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+              </div>
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#000" }}>
+                  Candidate Email
+                </Typography.Title>
+                <Form.Item
+                  name="email"
+                  className=""
+                  rules={[
+                    {
+                      required: true,
+                      message: "Email is Required",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter your email"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+              </div>
+            </div>
+
+            <div className="flex gap-5 justify-between">
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#000" }}>
+                  TikTok Link
+                </Typography.Title>
+                <Form.Item
+                  name="tikTokLink"
+                  className=""
+                  rules={[
+                    {
+                      required: true,
+                      message: "TikTok Link is Required",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter your TikTok Link"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+              </div>
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#000" }}>
+                  Followers
+                </Typography.Title>
+                <Form.Item
+                  name="followers"
+                  className=""
+                  rules={[
+                    {
+                      required: true,
+                      message: "followers is Required",
+                    },
+                  ]}
+                >
+                  <Input
+                    controls={false}
+                    placeholder="Enter your followers"
+                    className="py-1 px-3 text-xl bg-site-color border !border-tok-color w-full"
+                  />
+                </Form.Item>
+              </div>
+            </div>
 
             <Typography.Title level={4} style={{ color: "#222222" }}>
               Candidate Image
@@ -175,7 +262,7 @@ const AddCandidatesFrom = ({
               </Dragger>
             </Form.Item>
 
-            <Typography.Title level={4} style={{ color: "#222222" }}>
+            {/* <Typography.Title level={4} style={{ color: "#222222" }}>
               TikTok Link
             </Typography.Title>
             <Form.Item
@@ -192,9 +279,34 @@ const AddCandidatesFrom = ({
                 placeholder="Enter TikTok Link"
                 className=" px-3 text-xl border !border-input-color !bg-transparent "
               />
-            </Form.Item>
+            </Form.Item> */}
 
-            <Typography.Title level={4} style={{ color: "#222222" }}>
+            <div className="flex gap-5 justify-between">
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#fff" }}>
+                  Instagram Link
+                </Typography.Title>
+                <Form.Item name="instagramLink" className="">
+                  <Input
+                    placeholder="Enter your Instagram Link"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+              </div>
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#fff" }}>
+                  Facebook Link
+                </Typography.Title>
+                <Form.Item name="facebookLink" className="">
+                  <Input
+                    placeholder="Enter your Facebook Link"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+              </div>
+            </div>
+
+            {/* <Typography.Title level={4} style={{ color: "#222222" }}>
               Instagram Link
             </Typography.Title>
             <Form.Item
@@ -215,23 +327,14 @@ const AddCandidatesFrom = ({
             <Typography.Title level={4} style={{ color: "#222222" }}>
               Facebook Link
             </Typography.Title>
-            <Form.Item
-              // rules={[
-              //   {
-              //     required: true,
-              //     message: "Please Enter Candidate Facebook Link",
-              //   },
-              // ]}
-              name="facebookLink"
-              className=" "
-            >
+            <Form.Item name="facebookLink" className=" ">
               <Input
                 placeholder="Enter Facebook link"
                 className=" px-3 text-xl border !border-input-color !bg-transparent "
               />
-            </Form.Item>
+            </Form.Item> */}
 
-            <Typography.Title level={4} style={{ color: "#222222" }}>
+            {/* <Typography.Title level={4} style={{ color: "#222222" }}>
               Followers
             </Typography.Title>
             <Form.Item
@@ -249,7 +352,7 @@ const AddCandidatesFrom = ({
                 placeholder="Enter Candidate Followers Number"
                 className=" px-3 w-full text-xl border !border-input-color !bg-transparent "
               />
-            </Form.Item>
+            </Form.Item> */}
 
             <Typography.Title level={4} style={{ color: "#222222" }}>
               Bio
@@ -267,7 +370,24 @@ const AddCandidatesFrom = ({
               <TextArea
                 rows={2}
                 placeholder="Enter Candidate Bio"
-                className="py-2 px-3 text-xl border !border-input-color !bg-transparent "
+                className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+              />
+            </Form.Item>
+            <Typography.Title level={4} style={{ color: "#fff" }}>
+              Password
+            </Typography.Title>
+            <Form.Item
+              rules={[
+                {
+                  required: true,
+                  message: "Password is Required",
+                },
+              ]}
+              name="password"
+            >
+              <Input.Password
+                placeholder="Enter Candidate password"
+                className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
               />
             </Form.Item>
 
--- src/Components/SuperAdminPages/VotingCandidatePage/EditCandidateModal.jsx
+++ src/Components/SuperAdminPages/VotingCandidatePage/EditCandidateModal.jsx
@@ -34,6 +34,7 @@ const EditCandidateModal = ({
         facebookLink: currentVenueRecord.facebookLink,
         followers: currentVenueRecord.followers,
         bio: currentVenueRecord.bio,
+
         profileImage: currentVenueRecord.profileImage
           ? [
               {
@@ -131,7 +132,7 @@ const EditCandidateModal = ({
             layout="vertical"
             className="bg-transparent w-full text-start"
           >
-            <Typography.Title level={4} style={{ color: "#222222" }}>
+            {/* <Typography.Title level={4} style={{ color: "#222222" }}>
               Candidate Name
             </Typography.Title>
             <Form.Item
@@ -142,16 +143,105 @@ const EditCandidateModal = ({
                 },
               ]}
               name="name"
+              className=" "
             >
               <Input
                 placeholder="Enter Candidate name"
-                className="px-3 text-xl border !border-input-color !bg-transparent"
+                className=" px-3 text-xl border !border-input-color !bg-transparent"
               />
-            </Form.Item>
+            </Form.Item> */}
+
+            {/* <div className="flex gap-5 justify-between"> */}
+         
+                <Typography.Title level={4} style={{ color: "#000" }}>
+                  Candidate Name
+                </Typography.Title>
+                <Form.Item
+                  name="name"
+                  className=""
+                  rules={[
+                    {
+                      required: true,
+                      message: "Name is Required",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter your name"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+      
+              {/* <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#000" }}>
+                  Candidate Email
+                </Typography.Title>
+                <Form.Item
+                  name="email"
+                  className=""
+                  rules={[
+                    {
+                      required: true,
+                      message: "Email is Required",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter your email"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+              </div> */}
+            {/* </div> */}
+
+            <div className="flex gap-5 justify-between">
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#000" }}>
+                  TikTok Link
+                </Typography.Title>
+                <Form.Item
+                  name="tikTokLink"
+                  className=""
+                  rules={[
+                    {
+                      required: true,
+                      message: "TikTok Link is Required",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter your TikTok Link"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+              </div>
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#000" }}>
+                  Followers
+                </Typography.Title>
+                <Form.Item
+                  name="followers"
+                  className=""
+                  rules={[
+                    {
+                      required: true,
+                      message: "followers is Required",
+                    },
+                  ]}
+                >
+                  <Input
+                    controls={false}
+                    placeholder="Enter your followers"
+                    className="py-1 px-3 text-xl bg-site-color border !border-tok-color w-full"
+                  />
+                </Form.Item>
+              </div>
+            </div>
 
             <Typography.Title level={4} style={{ color: "#222222" }}>
               Candidate Image
             </Typography.Title>
+
             <Form.Item
               rules={[
                 {
@@ -160,6 +250,7 @@ const EditCandidateModal = ({
                 },
               ]}
               name="profileImage"
+              className=" "
             >
               <Dragger name="files" maxCount={1} action={false}>
                 <p className="ant-upload-drag-icon">
@@ -178,6 +269,7 @@ const EditCandidateModal = ({
             >
               Candidate Background Image
             </Typography.Title>
+
             <Form.Item
               rules={[
                 {
@@ -186,6 +278,7 @@ const EditCandidateModal = ({
                 },
               ]}
               name="backgroundImage"
+              className=" "
             >
               <Dragger name="files" maxCount={1} action={false}>
                 <p className="ant-upload-drag-icon">
@@ -197,7 +290,7 @@ const EditCandidateModal = ({
               </Dragger>
             </Form.Item>
 
-            <Typography.Title level={4} style={{ color: "#222222" }}>
+            {/* <Typography.Title level={4} style={{ color: "#222222" }}>
               TikTok Link
             </Typography.Title>
             <Form.Item
@@ -208,34 +301,68 @@ const EditCandidateModal = ({
                 },
               ]}
               name="tikTokLink"
+              className=" "
             >
               <Input
                 placeholder="Enter TikTok Link"
-                className="px-3 text-xl border !border-input-color !bg-transparent"
+                className=" px-3 text-xl border !border-input-color !bg-transparent "
               />
-            </Form.Item>
+            </Form.Item> */}
 
-            <Typography.Title level={4} style={{ color: "#222222" }}>
+            <div className="flex gap-5 justify-between">
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#fff" }}>
+                  Instagram Link
+                </Typography.Title>
+                <Form.Item name="instagramLink" className="">
+                  <Input
+                    placeholder="Enter your Instagram Link"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+              </div>
+              <div className="w-full">
+                <Typography.Title level={4} style={{ color: "#fff" }}>
+                  Facebook Link
+                </Typography.Title>
+                <Form.Item name="facebookLink" className="">
+                  <Input
+                    placeholder="Enter your Facebook Link"
+                    className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+                  />
+                </Form.Item>
+              </div>
+            </div>
+
+            {/* <Typography.Title level={4} style={{ color: "#222222" }}>
               Instagram Link
             </Typography.Title>
-            <Form.Item name="instagramLink">
+            <Form.Item
+              // rules={[
+              //   {
+              //     required: true,
+              //     message: "Please Enter Candidate instagram Link",
+              //   },
+              // ]}
+              name="instagramLink"
+              className=" "
+            >
               <Input
-                placeholder="Enter instagram Link"
-                className="px-3 text-xl border !border-input-color !bg-transparent"
+                placeholder="Enter instagram link"
+                className=" px-3 text-xl border !border-input-color !bg-transparent "
               />
             </Form.Item>
-
             <Typography.Title level={4} style={{ color: "#222222" }}>
               Facebook Link
             </Typography.Title>
-            <Form.Item name="facebookLink">
+            <Form.Item name="facebookLink" className=" ">
               <Input
-                placeholder="Enter facebook Link"
-                className="px-3 text-xl border !border-input-color !bg-transparent"
+                placeholder="Enter Facebook link"
+                className=" px-3 text-xl border !border-input-color !bg-transparent "
               />
-            </Form.Item>
+            </Form.Item> */}
 
-            <Typography.Title level={4} style={{ color: "#222222" }}>
+            {/* <Typography.Title level={4} style={{ color: "#222222" }}>
               Followers
             </Typography.Title>
             <Form.Item
@@ -246,13 +373,14 @@ const EditCandidateModal = ({
                 },
               ]}
               name="followers"
+              className=" "
             >
               <InputNumber
                 controls={false}
                 placeholder="Enter Candidate Followers Number"
-                className="px-3 w-full text-xl border !border-input-color !bg-transparent"
+                className=" px-3 w-full text-xl border !border-input-color !bg-transparent "
               />
-            </Form.Item>
+            </Form.Item> */}
 
             <Typography.Title level={4} style={{ color: "#222222" }}>
               Bio
@@ -265,20 +393,38 @@ const EditCandidateModal = ({
                 },
               ]}
               name="bio"
+              className=" "
             >
               <TextArea
                 rows={2}
                 placeholder="Enter Candidate Bio"
-                className="py-2 px-3 text-xl border !border-input-color !bg-transparent"
+                className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
               />
             </Form.Item>
+            {/* <Typography.Title level={4} style={{ color: "#fff" }}>
+              Password
+            </Typography.Title>
+            <Form.Item
+              rules={[
+                {
+                  required: true,
+                  message: "Password is Required",
+                },
+              ]}
+              name="password"
+            >
+              <Input.Password
+                placeholder="Enter Candidate password"
+                className="py-2 px-3 text-xl bg-site-color border !border-tok-color "
+              />
+            </Form.Item> */}
 
             <Form.Item>
               <Button
                 className="w-full py-6 border !border-secondary-color hover:border-secondary-color text-xl !text-primary-color bg-secondary-color hover:!bg-secondary-color font-semibold rounded mt-3"
                 htmlType="submit"
               >
-                Update
+               Update
               </Button>
             </Form.Item>
           </Form>
